0681952933edbadbb54eca0b729975254f640abc
humkeyg <87458845+humkeyg@users.noreply.github.com>
2021-12-09 18:42:42 -0800

    initial commit

---
+++ controllers/api/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const userRoutes = requires('./userRoutes.js');
+const blogRoutes = require('./blogRoutes.js');
+const commentsRoutes = require('./commentsRoutes.js');
+
+router.use('/user', userRoutes);
+router.use('/blog', blogRoutes);
+router.use('/comments', commentsRoutes);
+
+module.exports = router;
